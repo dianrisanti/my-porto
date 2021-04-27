888f3b7ffbca47f9581a9f8113865163ca02cb4e
dianrisanti <diannnyyy@gmail.com>
2021-04-27 13:53:59 +0700

    project changed

--- src/components/project.js
+++ src/components/project.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import {
-    Image
+    Image,
+    Carousel
 } from 'react-bootstrap'
-import { ProjectFoto } from '../assets'
+import '../css/socmed.css'
+import { Project1, Project2, Project3, Project4, Project5, Project6 } from '../assets'
 import Aos from 'aos'
 
 const Project = () => {
@@ -10,12 +12,19 @@ const Project = () => {
         Aos.init({ duration: 2000 })
     }, [])
 
+    const photos = [Project1, Project2, Project3, Project4, Project5, Project6]
+
     return(
         <div style={{height: "100vh"}} id="projects">
             <h2 style={{color: "#778da9"}} data-aos="fade-right" >My simple project I've built</h2>
             <div style={{display: "flex", flexDirection: "row", width: "75vw", justifyContent: "space-between", alignItems: "center", marginTop: 40}}>
                 <div style={{marginRight: 20, color: "#e5e5e5", marginTop: -80}} data-aos="fade-right">
-                    <h4 style={{marginBottom: 20}}>E-commerce</h4>
+                    <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between", padding: 10}}>
+                        <h4 style={{marginBottom: 20}}>E-commerce</h4>
+                        <a href="https://github.com/dianrisanti/ecommerce.git">
+                            <i className="fad fa-external-link-alt logo"></i>
+                        </a>
+                    </div>
                     <div style={{backgroundColor: "#1b263b", width: "400px", padding: 15, borderRadius: 5}}>
                         <p>A web application for buying and selling products with different warehouse locations. 
                         You could buy any available products and we would send them from the nearest warehouse location.</p>
@@ -24,15 +33,26 @@ const Project = () => {
                         <p>React</p>
                         <p>Node.js</p>
                         <p>Express</p>
+                        <p>MySQL</p>
                     </div>
                 </div>
                 <div data-aos="fade-left">
-                    <a href="https://github.com/dianrisanti/ecommerce.git">
-                        <Image
-                            src={ProjectFoto.default}
-                            style={{borderRadius: 5}}
-                        />
-                    </a>
+                    <Carousel>
+                        {
+                            photos.map((item, index) => {
+                                return(
+                                    <Carousel.Item key={index}>
+                                        <Image
+                                            className="d-block w-100"
+                                            src={item.default}
+                                            alt="First slide"
+                                            style={{height: "20rem", width: "52rem"}}
+                                        />
+                                    </Carousel.Item>
+                                )
+                            })
+                        }
+                    </Carousel>
                 </div>
             </div>
         </div>
